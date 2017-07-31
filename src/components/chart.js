7585a3e595b0a52c4973c460b8330c44d7d38a78
Alvin van der Kuech <vanderkuech@icloud.com>
2017-07-31 17:10:56 -0400

    Add chart, Add new weather data, lodash, doin weather avg

---
+++ src/components/chart.js
@@ -0,0 +1,21 @@
+import _ from 'lodash';
+import React from 'react';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+
+const average = (data) => {
+    return (
+
+    )
+}
+
+export default (props) => {
+    return (
+        <div>
+            <Sparklines height={120} width={180} data={props.data}>
+                <SparklinesLine color={props.color} />
+                <SparklinesReferenceLine type="avg" />   
+            </Sparklines>
+            <div>{average(props.data)}</div>
+        </div>
+    );
+}
